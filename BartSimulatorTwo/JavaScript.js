e6c69fa12371eabdaac29a4688df0a6a82663fb8
Alexandros Nicolaou <alnicolaou96@gmail.com>
2019-01-28 14:06:07 -0500

    javascript start button opens up player select

--- BartSimulatorTwo/JavaScript.js
+++ BartSimulatorTwo/JavaScript.js
@@ -1,3 +1,10 @@
 ﻿function StartGame() {
-    console.log("bjsvojdb")
+    console.log("bjsvojdb");
+    var x = document.getElementById("onePlayer");
+    var y = document.getElementById("twoPlayer");
+    var z = document.getElementById("logo");
+
+    x.style.display = "flex";
+    y.style.display = "flex";
+    z.style.display = "none";
 }
